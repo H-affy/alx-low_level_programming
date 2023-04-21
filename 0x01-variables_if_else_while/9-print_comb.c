32830857bbb1fafc4f0300817e1420137d5ee9a4
H-affy <abdullateefadeluola@gmail.com>
2023-04-21 15:03:39 -0700

    Comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,12 @@ int main(void)
 {
 	int n;
 
-	for (n = 45; n < 58; n++)
-
+	for (n = 48; n <= 57; n++)
+	{
 		putchar(n);
-	if (n != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-		putchar('\n');
-		return (0);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
 }
