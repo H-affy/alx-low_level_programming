d5d2f8bc910891fee257422c1f2d337c6c51e629
H-affy <abdullateefadeluola@gmail.com>
2023-04-26 10:44:14 -0700

    Is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - _isalpha show 1 if is a letter
+ * Another  cases, show 0
+ *
+ * Return: Always 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
