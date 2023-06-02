1db6f842714275829fcdfb742ed3c2b97ffbf182
H-affy <abdullateefadeluola@gmail.com>
2023-06-01 17:39:46 -0700

    First

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * first - a function that prints a sentence before the main
+ *
+ * Function is executed.
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("i bore my house upon my back!\n");
+}
