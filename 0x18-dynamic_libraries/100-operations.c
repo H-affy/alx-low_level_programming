2514bc1dcd438131129505dcaf284a20a6549db7
H-affy <abdullateefadeluola@gmail.com>
2023-08-21 13:49:41 -0700

    Operation

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - Sum of two int
+ * @a: number to sum
+ * @b: number to sum
+ *
+ * Return: Sum of two number
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - difference between two number
+ * @a: number
+ * @b: number
+ *
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplication
+ * @a: number
+ * @b: number
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * div - division
+ * @a: number
+ * @b: number
+ *
+ * Return: result
+ */
+int div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * mod - difference between two number
+ * @a: number
+ * @b: number
+ *
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
